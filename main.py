42c574114e220b9c7eb6ad130c094842fd5de6e6
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-24 11:42:22 +0000

    Get the tv titles

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-"""Scraping Empire Website for the article on the best tv shows ever"""
+"""Scraping Stacker Website for the article on the best 100 tv shows ever"""
 
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.empireonline.com/tv/features/best-tv-shows-ever-2/')
+STACKER_URL = 'https://stacker.com/stories/980/100-best-tv-shows-all-time'
+
+response = requests.get(STACKER_URL)
 response.raise_for_status()
-data = response.text
+website_data = response.text
+
+soup = BeautifulSoup(website_data, "html.parser")
+tv_shows = soup.find_all(name="h2")
+tv_titles = [tv.getText() for tv in tv_shows[3:-1]]
+print(tv_titles)
